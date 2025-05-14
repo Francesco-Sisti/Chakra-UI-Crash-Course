1821266ef3075c5e4f723eab7c23aec9eb278c1f
Francesco-sisti <francesco.s@netstrategy.it>
2025-05-14 11:49:55 +0200

    Ristrutturazione del layout principale con Chakra UI e aggiornamento della dashboard per utilizzare SimpleGrid e Box.

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,21 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { Grid, GridItem, Text } from "@chakra-ui/react"
 export default function RootLayout() {
   return (
     <div>
-      <Navbar />
-      <Outlet />
+      <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+
+        <GridItem as="aside" bg="purple.400" minHeight={{lg: '100vh'}} p={{base: '20px', lg: '30px'}} colSpan={{base: 6, lg: 2, xl: 1}}>
+          <span>Sidebar</span>
+        </GridItem>
+
+        <GridItem as="main" colSpan={{base: 6, lg: 4, xl: 5}} p={{base: '20px', lg: '30px'}}>
+          <Navbar />
+          <Outlet />
+        </GridItem>
+
+      </Grid>
     </div>
   )
 }
